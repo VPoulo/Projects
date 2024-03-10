4994712d4f5cb7c28fa777d73d0b74fb361151c6
Vaughn <85858460+VPoulo@users.noreply.github.com>
2024-03-09 21:22:25 -0800

    Update concept.h

--- B-Tree/concept.h
+++ B-Tree/concept.h
@@ -17,35 +17,35 @@ class concept
 {
 public:
 	concept();
-	virtual ~concept();				  // Virtual Destructor.
-	virtual void display();			  // Displays function.
-	virtual void select() = 0;		  // WHAT SHOULD THIS DO?
-	virtual void getInformation();	  // Prompts the user to read information.
-	virtual void changeRating() = 0;  // Changes rating of concept.
-	bool operator==(string);		  // Compares keyword for equality.
-	bool operator!=(string);		  // Compares keyword for inequality.
-	bool operator<=(const concept &); // Compares keyword for alphabetical order.
-	bool operator<(const concept &);  // Compare keyword for alphabetical order.
-	bool operator>=(const concept &); // Compares keyword for alphabetical order.
-	bool operator>(const concept &);  // Compares keyword for alphabetical order.
+	virtual ~concept();			// Virtual Destructor.
+	virtual void display();			// Displays function.
+	virtual void select() = 0;
+	virtual void getInformation();	  	// Prompts the user to read information.
+	virtual void changeRating() = 0;  	// Changes rating of concept.
+	bool operator==(string);		// Compares keyword for equality.
+	bool operator!=(string);		// Compares keyword for inequality.
+	bool operator<=(const concept &); 	// Compares keyword for alphabetical order.
+	bool operator<(const concept &);  	// Compare keyword for alphabetical order.
+	bool operator>=(const concept &); 	// Compares keyword for alphabetical order.
+	bool operator>(const concept &);  	// Compares keyword for alphabetical order.
 
 protected:
-	string keyword;		 // Name of Concept.
-	string url;			 // Website for concept.
-	float rating;		 // Concept Difficulty.
-	string concept_type; // Type of concept added.
+	string keyword;		// Name of Concept.
+	string url;		// Website for concept.
+	float rating;		// Concept Difficulty.
+	string concept_type; 	// Type of concept added.
 };
 
 class stl_syntax : public concept
 {
 public:
-	stl_syntax();		   // Constructor.
-	~stl_syntax();		   // Destructor.
-	void display();		   // Displays function.
-	void select();		   // WHAT SHOULD THIS DO?
-	void getInformation(); // Prompts the user to read information.
-	void changeRating();   // Changes rating of concept.
-	void addMethod();	   // Adds method to list.
+	stl_syntax();		// Constructor.
+	~stl_syntax();		// Destructor.
+	void display();		// Displays function.
+	void select();	
+	void getInformation();	// Prompts the user to read information.
+	void changeRating();	// Changes rating of concept.
+	void addMethod();	// Adds method to list.
 
 private:
 	vector<method_container> methods; // List of methods.
@@ -54,14 +54,14 @@ private:
 class modern_concept : public concept
 {
 public:
-	modern_concept();	   // Constructor.
-	~modern_concept();	   // Destructor.
-	void display();		   // Displays function.
-	void select();		   // WHAT SHOULD THIS DO?
-	void getInformation(); // Prompts the user to read information.
-	void changeRating();   // Changes rating of concept.
-	void addBenefit();	   // Add benefit to concept.
-	void addDrawback();	   // Add drawback to concept.
+	modern_concept();	// Constructor.
+	~modern_concept();	// Destructor.
+	void display();		// Displays function.
+	void select();	
+	void getInformation();	// Prompts the user to read information.
+	void changeRating();	// Changes rating of concept.
+	void addBenefit();	// Add benefit to concept.
+	void addDrawback();	// Add drawback to concept.
 
 private:
 	string desc;	 // Concept Description.
@@ -72,13 +72,13 @@ private:
 class python_library : public concept
 {
 public:
-	python_library();	   // Constructor.
-	~python_library();	   // Destructor.
-	void display();		   // Displays function.
-	void select();		   // WHAT SHOULD THIS DO?
+	python_library();	// Constructor.
+	~python_library();	// Destructor.
+	void display();		// Displays function.
+	void select();
 	void getInformation(); // Prompts the user to read information.
 	void changeRating();   // Changes rating of concept.
-	void addMethod();	   // Adds method to list.
+	void addMethod();	// Adds method to list.
 
 private:
 	string desc;						 // Concept Description.
